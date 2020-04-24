eb0d35a0530a76846956989ca3976043b0023e41
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2020-04-23 17:08:20 -0700

    ee
    ee

---
+++ install_p37.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt install python3.7
+sudo apt install python3-pip
+alias python=python3.7
+alias pip=pip3
+python -V
