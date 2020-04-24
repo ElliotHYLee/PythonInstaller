a26391480bb6432680fed1c6d26ba3a5ade2a4b1
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2020-04-24 09:20:36 +0900

    Update install_p36.sh

--- install_p36.sh
+++ install_p36.sh
@@ -4,6 +4,6 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt install python3.6
 sudo apt install python3-pip
-alias python=python3.6
-alias pip=pip3
+echo 'alias python=python3.6 \n alias pip=pip3' > ~/.bash_aliases
+source ~/.bash_aliasesss
 python -V
