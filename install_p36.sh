00b6418208196ad3a90c20f9c0f1c5ce730cff65
el <hylee101001@gmail.com>
2020-04-23 17:31:04 -0700

    aaaa

--- install_p36.sh
+++ install_p36.sh
@@ -5,5 +5,5 @@ sudo apt-get update
 sudo apt install python3.6
 sudo apt install python3-pip
 echo 'alias python=python3.6 \n alias pip=pip3' > ~/.bash_aliases
-source ~/.bash_aliases
+. ~/.bash_aliases
 python -V
--- install_p37.sh
+++ install_p37.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
@@ -5,5 +6,5 @@ sudo apt-get update
 sudo apt install python3.7
 sudo apt install python3-pip
 echo 'alias python=python3.7 \n alias pip=pip3' > ~/.bash_aliases
-source ~/.bash_aliases
+. ~/.bash_aliases
 python -V
